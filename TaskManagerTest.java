6e68c99c1c46c40e8c54119f820d1781edff69b7
InduYekkala <indu12.cse@gmail.com>
2019-10-21 14:43:05 -0700

    added TestCase

---
+++ TaskManagerTest.java
@@ -0,0 +1,54 @@
+package com.novare.IndividualProject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskManagerTest {
+
+    @Test
+    public void testSortProjectName() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testSortDueDate() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testFindTask() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testListOfTasks() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testIsDuplicateTask() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testRemoveTask() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testEditTaskStatus() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testEditTaskTitle() {
+	fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    public void testTaskStatusCount() {
+	fail("Not yet implemented"); // TODO
+    }
+
+}
