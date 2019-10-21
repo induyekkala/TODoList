eaea077a4ac2405939ca017717340b07bea4ae80
InduYekkala <indu12.cse@gmail.com>
2019-10-21 15:09:52 -0700

    added Testcase for sort by project name

--- TaskManagerTest.java
+++ TaskManagerTest.java
@@ -2,53 +2,57 @@ package com.novare.IndividualProject;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 import org.junit.Test;
 
 public class TaskManagerTest {
-
+    
+    
+    private Path file_Path = new File("/Users/induyekkala/ToDoList.csv").toPath();
+    private Charset charset = Charset.defaultCharset();
+    private ArrayList<UserTask> listOfTasks = new ArrayList<UserTask>();
+    private TaskManager taskManager=new TaskManager();
     @Test
     public void testSortProjectName() {
-	fail("Not yet implemented"); // TODO
-    }
+	
+	
+	String[] userTaskArray = { "" };
+	try {
 
-    @Test
-    public void testSortDueDate() {
-	fail("Not yet implemented"); // TODO
-    }
+	    List<String> taskList = Files.readAllLines(file_Path, charset);
+	    if (taskList != null) {
+		taskList.remove(0);
+		userTaskArray = taskList.toArray(new String[] {});
+		Arrays.sort(userTaskArray, new Comparator<String>() {
+		    public int compare(String firstProjectName, String secondProjectName) {
+			return (firstProjectName.split(",")[4]).compareTo((secondProjectName.split(",")[4]));
 
-    @Test
-    public void testFindTask() {
-	fail("Not yet implemented"); // TODO
+		    }
+		});
+	    }
+	}
+	catch(Exception e)
+	{
+	    e.printStackTrace();
+	}
+	String arr[]= {"apple","mango"};
+	String exceptedUserTaskArray[]=taskManager.sortProjectName();
+	
+	assertTrue(Arrays.equals(arr,userTaskArray));
     }
+    
 
-    @Test
-    public void testListOfTasks() {
-	fail("Not yet implemented"); // TODO
-    }
+    
+    
 
-    @Test
-    public void testIsDuplicateTask() {
-	fail("Not yet implemented"); // TODO
-    }
-
-    @Test
-    public void testRemoveTask() {
-	fail("Not yet implemented"); // TODO
-    }
-
-    @Test
-    public void testEditTaskStatus() {
-	fail("Not yet implemented"); // TODO
-    }
-
-    @Test
-    public void testEditTaskTitle() {
-	fail("Not yet implemented"); // TODO
-    }
-
-    @Test
-    public void testTaskStatusCount() {
-	fail("Not yet implemented"); // TODO
-    }
+    
 
 }
